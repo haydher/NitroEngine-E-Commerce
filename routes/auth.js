9d6a88674e4c62a87d864747136ee33d4aa90078
haydher <haider0612@gmail.com>
2021-04-21 00:22:58 -0300

    updated auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -53,7 +53,7 @@ router.use(
   secret: process.env.SECRET_SESSION_TOKEN,
   resave: false,
   saveUninitialized: false,
-  store: MongoStore.create({ mongoUrl: "mongodb://localhost/NitroEngine" }),
+  store: MongoStore.create({ mongoUrl: `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@nitroengine.kgaw6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority` }),
  })
 );
 router.use(passport.initialize());
