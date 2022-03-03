97a35c5c6bd8062b936bbd158b3f6d238dc336de
Ali Haider <haider0612@gmail.com>
2022-03-03 16:46:50 -0500

    added a ReadMe

--- routes/catalog.js
+++ routes/catalog.js
@@ -15,16 +15,6 @@ router.post("/", async (req, res) => {
 
  const correctData = validateInput(req.body);
  if (correctData.error != undefined) return res.status(400).send(correctData.error.details[0].message);
-
- // looks for duplicates
- // if (catalog.length > 0) {
- //  if (catalog[0].type != undefined) {
- //   for (i = 0; i < catalog.length; i++) {
- //    if (catalog[i].type == newType) return res.status(400).send(`type of ${newType} already exists`);
- //   }
- //  }
- // }
-
  try {
   // this saves the data into the database
   newObj = await newObj.save();
@@ -72,9 +62,6 @@ router.get("/id/:id", async (req, res) => {
 router.put("/id/:id", async (req, res) => {
  const paramID = `${req.params.id}`;
 
- // let catalog = await Catalog.findByIdAndUpdate(paramID, { $set: req.body }, { new: true });
- // console.log("cataloggggggggg", catalog);
- // if (!catalog) return res.status(400).send(`No item with the ID of ${req.params.id} exists`);
  try {
   catalog = await Catalog.findByIdAndUpdate(paramID, { $set: req.body }, { new: true });
  } catch (error) {
@@ -95,152 +82,3 @@ router.delete("/id/:id", async (req, res) => {
 });
 
 module.exports = router;
-
-/*
-
-const mongoose = require("mongoose");
-const Joi = require("joi");
-const express = require("express");
-const router = express.Router();
-
-const catalog = [];
-
-// holds the info about how the database should look like
-
-const Catalog = mongoose.model(
- "Catalog",
- new mongoose.Schema({
-  // makes sure the user has the name
-  name: {
-   type: String,
-   required: true,
-   // this converts the string to lowercase
-   lowerCase: true,
-   // set the length of the string. (characters)
-   minLength: 5,
-   maxLength: 30,
-  },
-  category: {
-   type: String,
-   lowercase: true,
-  },
-  tags: {
-   type: Array,
-   validate: {
-    validator: function (v) {
-     return v && v.length > 0;
-    },
-    message: "Tags should have at least one value",
-   },
-  },
-  date: { type: Date, default: Date.now },
-  price: {
-   type: Number,
-   min: 10,
-   max: 200,
-   required: true,
-  },
- })
-);
-
-router.get("/", async (req, res) => {
- // if (catalog[0].type != undefined) console.log(catalog[0].type);
- const catalog = await Catalog.find();
- res.send(catalog);
-});
-
-router.post("/", (req, res) => {
- const newType = `${req.body.type}`.toLowerCase();
-
- const result = validateInput({ type: req.body.type });
-
- // if error doesnt equal undefined or means there is an error then abort and show message
- if (result.error != undefined) return res.status(400).send(result.error.details[0].message);
-
- // looks for duplicates
- if (catalog.length > 0) {
-  if (catalog[0].type != undefined) {
-   for (i = 0; i < catalog.length; i++) {
-    if (catalog[i].type == newType) return res.status(400).send(`type of ${newType} already exists`);
-   }
-  }
- }
-
- const newObj = {
-  type: newType,
-  [newType]: [],
- };
- // push it in the obj
- catalog.push(newObj);
- // show the user the result
- res.send(catalog);
-});
-
-// make new content for the type
-router.get("/:type/", (req, res) => {
- const params = `${req.params.type}`.toLowerCase();
- const type = catalog[0][params];
- res.send(type);
-});
-
-router.post("/:type/", (req, res) => {
- // gets the type from url
- const params = `${req.params.type}`.toLowerCase();
- const type = catalog[0][params];
-
- // make new obj in the specified type
- const itemDetail = {
-  id: type.length + 1,
-  price: req.body.price,
-  name: req.body.name,
-  gender: req.body.gender,
-  description: req.body.description,
-  gender: req.body.gender,
-  image: [],
- };
-
- const correctData = checkTypeData(itemDetail);
- if (correctData.error != undefined) return res.status(400).send(correctData.error.details[0].message);
-
- for (let i = 0; i < type.length; i++) {
-  if (req.body.name == type[0].name) return res.status(400).send(`This name already exists for item : ${type[0].id}`);
-  if (req.body.description == type[0].name)
-   return res.status(400).send(`This description already exists for item : ${type[0].id}`);
- }
-
- type.push(itemDetail);
- res.send(catalog);
-});
-
-router.get("/:type/:id", (req, res) => {
- res.send(catalog);
-});
-
-// validate the input
-function validateInput(course) {
- // get the type
- // console.log(Object.keys(course)[0]);
-
- const schema = Joi.object({
-  [Object.keys(course)[0]]: Joi.string().min(3).required(),
- });
- return schema.validate(course);
-}
-
-function checkTypeData(params) {
- const schema = Joi.object({
-  id: Joi.number().positive(),
-  price: Joi.number().positive().precision(2).required(),
-  name: Joi.string().min(5).max(28).required(),
-  gender: Joi.string().min(4).required(),
-  description: Joi.string().required(),
-  image: Joi.array(),
-  // image: Joi.array().min(3).unique(),
- });
- return schema.validate(params);
-}
-
-module.exports = router;
-
-
-*/
--- routes/receipt.js
+++ routes/receipt.js
@@ -1,6 +1,6 @@
 const { User, PrevOrder } = require("../models/user");
 const { Item } = require("../models/item");
-const {authToken} = require("../middleware/authToken");
+const { authToken } = require("../middleware/authToken");
 const { reqLoginTrue } = require("../middleware/authUser");
 const admin = require("../middleware/admin");
 const express = require("express");
@@ -11,99 +11,98 @@ const router = express.Router();
 router.use(express.urlencoded({ extended: true }));
 
 router.get("/", [authToken, reqLoginTrue], async (req, res) => {
- 
- fullURL = req.header('Referer') || '/';
- const urlArr = fullURL.split("/")
- const urlPath = urlArr[urlArr.length - 1]
- console.log("urlPath",urlPath)
- // if(urlPath != "checkout" || urlPath != "receipt") {
- //  console.log("Redirecting because not valid path", urlPath)
- //  return res.redirect("/")
- // }
- const user = await User.findById(req.userId)
- if(!user || user.length < 1) return redirect("/")
+ fullURL = req.header("Referer") || "/";
+ const urlArr = fullURL.split("/");
+ const urlPath = urlArr[urlArr.length - 1];
+ console.log("urlPath", urlPath);
 
- const itemArr = [], itemSize = []
- for(let i = 0; i < user.prevOrders.length; i++){
-  const itemId = user.prevOrders[i].itemId
-  const item = await Item.findById(itemId)
-  if(!item || item?.length < 1) return res.redirect("/")
-  itemArr.push(item)
-  itemSize.push(user.prevOrders[i].size)
+ const user = await User.findById(req.userId);
+ if (!user || user.length < 1) return redirect("/");
+
+ const itemArr = [],
+  itemSize = [];
+ for (let i = 0; i < user.prevOrders.length; i++) {
+  const itemId = user.prevOrders[i].itemId;
+  const item = await Item.findById(itemId);
+  if (!item || item?.length < 1) return res.redirect("/");
+  itemArr.push(item);
+  itemSize.push(user.prevOrders[i].size);
  }
- res.render("receipt", { user, itemArr, itemSize })
+ res.render("receipt", { user, itemArr, itemSize });
 });
 
-router.post("/", [authToken, reqLoginTrue], async (req, res)=>{
- 
+router.post("/", [authToken, reqLoginTrue], async (req, res) => {
  let user = await User.findById(req.userId).select("-password");
- if(user == undefined) return res.status(404).render("404")
+ if (user == undefined) return res.status(404).render("404");
 
- let itemArr = []
- let itemArrId =  user.userCart[0]?.itemId
- let itemSize = user.userCart[0]?.size
+ let itemArr = [];
+ let itemArrId = user.userCart[0]?.itemId;
+ let itemSize = user.userCart[0]?.size;
 
- for(let i = 0; i < itemArrId.length; i++){
+ for (let i = 0; i < itemArrId.length; i++) {
+  let item = await Item.findById(itemArrId[i]);
 
-  let item = await Item.findById(itemArrId[i])
-
-  if(item != undefined || item?.length > 0) {
-
-   itemArr.push(item)
-   // if prev order is empty then add an item, 
-   if(user.prevOrders.length < 1){
-
-    await User.updateMany({ _id: req.userId },
-     {$set: {
-       "prevOrders": [new PrevOrder({
+  if (item != undefined || item?.length > 0) {
+   itemArr.push(item);
+   // if prev order is empty then add an item,
+   if (user.prevOrders.length < 1) {
+    await User.updateMany(
+     { _id: req.userId },
+     {
+      $set: {
+       prevOrders: [
+        new PrevOrder({
          userId: user._id,
          itemId: item,
          size: itemSize[0],
-       })],
+        }),
+       ],
       },
-     },{ useUnifiedTopology: true }
+     },
+     { useUnifiedTopology: true }
     );
-
-   }else if(user.prevOrders.length > 0){
-
-    for(let j = 0; j < user.prevOrders.length; j++){
-
-     if( !user.prevOrders[j].itemId == item && !user.prevOrders[j].size == itemSize[i])
-       user.prevOrders.push(new PrevOrder({
+   } else if (user.prevOrders.length > 0) {
+    for (let j = 0; j < user.prevOrders.length; j++) {
+     if (!user.prevOrders[j].itemId == item && !user.prevOrders[j].size == itemSize[i])
+      user.prevOrders.push(
+       new PrevOrder({
         userId: user._id,
         itemId: itemArrId[i],
-        size: itemSize[i],})
-       )
-     else console.log("prevOrder already has items that exists in the cart")
-
+        size: itemSize[i],
+       })
+      );
+     else console.log("prevOrder already has items that exists in the cart");
     }
    }
   }
  }
 
- await User.updateMany({ _id: req.userId },
-  {$set: {
-   "giftValue": updateGiftValue(itemArr, user.giftValue),
-   "userCart": []
+ await User.updateMany(
+  { _id: req.userId },
+  {
+   $set: {
+    giftValue: updateGiftValue(itemArr, user.giftValue),
+    userCart: [],
    },
-  },{ useUnifiedTopology: true }
+  },
+  { useUnifiedTopology: true }
  );
  try {
-  console.log("saving the user model")
-  await user.save()
-  console.log("Successfully saved the user model")
+  console.log("saving the user model");
+  await user.save();
+  console.log("Successfully saved the user model");
  } catch (error) {
-  console.log("Error saving model", error)
+  console.log("Error saving model", error);
  }
- res.clearCookie("cart").redirect("/receipt")
-})
+ res.clearCookie("cart").redirect("/receipt");
+});
 
-function updateGiftValue(itemArr, giftValue){
+function updateGiftValue(itemArr, giftValue) {
  let totalPrice = 0;
- itemArr.forEach(item => {
-  totalPrice += item.price
+ itemArr.forEach((item) => {
+  totalPrice += item.price;
  });
- return (giftValue - totalPrice)
+ return giftValue - totalPrice;
 }
 
 module.exports = router;
